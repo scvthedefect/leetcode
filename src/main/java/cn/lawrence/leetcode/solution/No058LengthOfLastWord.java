88ca63c22e4ace58df3b1f85335646a0903f1e5a
Lawrence Chen <lawrence.scv@gmail.com>
2019-02-27 19:04:47 +0800

    No.58 Length of last word

---
+++ src/main/java/cn/lawrence/leetcode/solution/No058LengthOfLastWord.java
@@ -0,0 +1,47 @@
+package cn.lawrence.leetcode.solution;
+
+public class No058LengthOfLastWord {
+
+    public static void main(String[] args) {
+        test("h", 1);
+        test("apple", 5);
+        test("hello world", 5);
+        test("hi world ", 5);
+        test(" bigFish ", 7);
+    }
+    
+    public static void test(String str, int len) {
+        Solution58 s = new Solution58();
+        int length = s.lengthOfLastWord(str);
+        System.out.println(String.format("input: %s; match: %b", str, length == len));
+    }
+    
+}
+
+class Solution58 {
+    
+    public int lengthOfLastWord(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int len = s.length();
+        int lastIndex = -1;
+        int secLastIndex = -1;
+        for(int i = len -1; i>= 0; i--) {
+            if(lastIndex == -1 && s.charAt(i) != ' ') {
+                lastIndex = i;
+            }
+            
+            if(secLastIndex == -1 && lastIndex != -1 && s.charAt(i) == ' ') {
+                secLastIndex = i;
+            }
+            
+            if(lastIndex != -1 && secLastIndex > 0) {
+                break;
+            }
+        }
+        return lastIndex - secLastIndex;
+    }
+    
+}
