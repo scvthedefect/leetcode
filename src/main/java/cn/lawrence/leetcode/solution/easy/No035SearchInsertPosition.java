7185d17a3594c63b2a4fd0be9f2b4890ec013a0c
Lawrence Chen <lawrence.scv@gmail.com>
2019-03-11 18:03:53 +0800

    二分法优化

--- src/main/java/cn/lawrence/leetcode/solution/easy/No035SearchInsertPosition.java
+++ src/main/java/cn/lawrence/leetcode/solution/easy/No035SearchInsertPosition.java
@@ -5,36 +5,72 @@ import cn.lawrence.leetcode.utils.ArrayUtil;
 public class No035SearchInsertPosition {
 
     public static void main(String[] args) {
-        test(1, 0, -1,0,1,2,3);
-        test(4, 7, 0,1,2,3);
+        int[] arr = {-1, 0, 1, 2, 3};
+        test(1, 0, arr);
+        test(5, 7, arr);
+        test(0, -2, arr);
+        
+        test(1, 2, 1,3);
     }
     
     public static void test(int expected, int target, int...nums) {
         Solution035 s = new Solution035();
         int index = s.searchInsert(nums, target);
-        boolean isExpected = expected == index;
-        ArrayUtil.printArr(nums);
-        System.out.println(String.format("Is expected: %b", isExpected));
         
-        // TODO 二分法改进
+        ArrayUtil.printArr(nums);
+        
+        boolean isExpected = expected == index;
+        System.out.println(String.format("[target num: %d], [expected: %d], [output: %d], [is expected: %b]", 
+                target, expected, index, isExpected));
     }
     
 }
 
 class Solution035 {
     
+//    public int searchInsert(int[] nums, int target) {
+//        if(nums == null || nums.length == 0) {
+//            return 0;
+//        }
+//        
+//        int pos = 0;
+//        for(; pos < nums.length; pos++) {
+//            if(nums[pos] >= target) {
+//                break;
+//            }
+//        }
+//        return pos;
+//    }
+    
     public int searchInsert(int[] nums, int target) {
+        int index = -1;
         if(nums == null || nums.length == 0) {
-            return 0;
+            return index;
         }
         
-        int pos = 0;
-        for(; pos < nums.length; pos++) {
-            if(nums[pos] >= target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        int val = 0;
+        boolean isMatch = false;
+        boolean isRight = false;
+        while(left <= right) {
+            isRight = false;
+            mid = left + (right - left) / 2;
+            val = nums[mid];
+            
+            
+            if(target == val) {
+                isMatch = true;
                 break;
+            } else if (target < val) {
+                right = mid - 1;
+            } else {
+                isRight = true;
+                left = mid + 1;
             }
         }
-        return pos;
+        return isMatch || !isRight ? mid : mid + 1;
     }
 }
 
--- src/main/java/cn/lawrence/leetcode/utils/ArrayUtil.java
+++ src/main/java/cn/lawrence/leetcode/utils/ArrayUtil.java
@@ -3,10 +3,16 @@ package cn.lawrence.leetcode.utils;
 public class ArrayUtil {
     
     public static void printArr(int[] nums) {
-        System.out.print("打印数组：");
-        for(int num : nums) {
+        System.out.print("[");
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
+            int num = nums[i];
             System.out.print(num);
+            if(i != len - 1) {
+                System.out.print(", ");
+            }
         }
+        System.out.print("]");
         System.out.println();
     }
 
