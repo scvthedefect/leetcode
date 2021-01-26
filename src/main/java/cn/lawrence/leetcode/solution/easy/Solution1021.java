5d7ae83f8d53e7619774c9136e4e467f55672733
Lawrence Chen <lawrence.scv@gmail.com>
2021-01-26 10:54:33 +0800

    new solution

--- src/main/java/cn/lawrence/leetcode/solution/easy/Solution1021.java
+++ src/main/java/cn/lawrence/leetcode/solution/easy/Solution1021.java
@@ -2,8 +2,39 @@ package cn.lawrence.leetcode.solution.easy;
 
 public class Solution1021 {
     
+    public static void main(String[] args) {
+        Solution1021 solution = new Solution1021();
+//        String S = "(()())(())";
+//        String S = "";
+//        String S = "(()())(())(()(()))";
+        String S = "()()";
+        String str = solution.removeOuterParentheses(S);
+        System.out.println(str);
+    }
+    
     public String removeOuterParentheses(String S) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        String str;
+        int count = 0;
+        int i = 0, j = 0;
+        char c;
+        
+        while (i < S.length()) {
+            c = S.charAt(i);
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            
+            if (count == 0) {
+                str = S.substring(j + 1, i);
+                sb.append(str);
+                j = i + 1;
+            }
+            i++;
+        }
+        return sb.toString();
     }
 
 }
---
+++ src/main/java/cn/lawrence/leetcode/solution/easy/Solution145.java
@@ -0,0 +1,55 @@
+package cn.lawrence.leetcode.solution.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution145 {
+    
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(1, null, 2, 3);
+//        TreeNode root = new TreeNode(1, 2, 3, 4, 5, 6, 7);
+//        TreeNode root = new TreeNode(1);
+        TreeNode root = null;
+        List<Integer> result = new Solution145().postorderTraversal(root);
+        System.out.println(result.toString());
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+        }
+        
+        TreeNode current, left, right;
+        TreeNode prev = null;
+        
+        boolean isHandled = false, isLeaf = false;
+        Integer val;
+        while (!stack.isEmpty()) {
+            current = stack.peek();
+            left = current.left;
+            right = current.right;
+            
+            isLeaf = left == null && right == null;
+            isHandled = prev != null && (left == prev || right == prev);
+            prev = current;
+            if (isLeaf || isHandled) {
+                val = Integer.valueOf(current.val);
+                result.add(val);
+                stack.pop();
+                continue;
+            }
+            
+            if (right != null) {
+                stack.push(right);
+            }
+            if (left != null) {
+                stack.push(left);
+            }
+        }
+        return result;
+    }
+
+}
